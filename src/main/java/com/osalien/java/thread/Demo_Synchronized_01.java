9ccf6dc9722b9ec658ba3a82ee83fce31503b73e
zhaoyuan <me@osalien.com>
2021-05-18 18:14:42 +0800

    同步代码块（内置锁） 案例：四个窗口售票

---
+++ src/main/java/com/osalien/java/thread/Demo_Synchronized_01.java
@@ -0,0 +1,23 @@
+package com.osalien.java.thread;
+
+public class Demo_Synchronized_01 {
+    /**
+     * @param args
+     * 需求:铁路售票,一共100张,通过四个窗口卖完.
+     */
+    public static void main(String[] args) {
+        TicketsSeller t1 = new TicketsSeller();
+        TicketsSeller t2 = new TicketsSeller();
+        TicketsSeller t3 = new TicketsSeller();
+        TicketsSeller t4 = new TicketsSeller();
+
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+        t3.setName("窗口3");
+        t4.setName("窗口4");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
---
+++ src/main/java/com/osalien/java/thread/TicketsSeller.java
@@ -0,0 +1,30 @@
+package com.osalien.java.thread;
+
+public class TicketsSeller extends Thread {
+    private static int tickets = 100;
+    static Object obj = new Object();
+    public TicketsSeller() {
+        super();
+
+    }
+    public TicketsSeller(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        while(true) {
+
+            synchronized(obj) {
+                if(tickets <= 0)
+                    break;
+                try {
+                    Thread.sleep(100);//线程1睡,线程2睡,线程3睡,线程4睡
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+                System.out.println(getName() + "...这是第" + tickets-- + "号票");
+            }
+        }
+    }
+}
